2497db02c17e55cdc65039711c7181aea154d595
imerofeev <imerofeev@gmail.com>
2020-03-22 19:40:48 +0300

    feat: add solvation of task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,17 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let brackets = bracketsConfig.map(function (element) {
+    return element.join('');
+  });
+  for (let i = 0; i < brackets.length; i++) {
+    if(str.includes(brackets[i])) {
+      str = str.replace(brackets[i], "");
+      i = -1;
+    }
+  }
+  if (str) {
+    return false;
+  } 
+  else {
+    return true;
+  }
 }
